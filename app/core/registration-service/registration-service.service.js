5f7db80e73564a64e78ec8040c12682ae88a5091
Christian Basile <cbasile@ymail.com>
2019-03-24 18:50:40 -0400

    add local storage to hold model upon page refresh

--- app/core/registration-service/registration-service.service.js
+++ app/core/registration-service/registration-service.service.js
@@ -6,15 +6,30 @@ angular.module('core.registration-service')
                 password: '',
                 confirmPassword: ''
             }
+
+            if(localStorage.registrationUserStorage) {
+                registration = JSON.parse(localStorage.registrationUserStorage);
+            } else {
+                updateLocalStorage()
+            }
+
+            function updateLocalStorage() {
+                localStorage.registrationUserStorage = JSON.stringify(registration);
+            }
+            
+            
             return {
                 setUsername: function(username) {
                     registration.username = username;
+                    updateLocalStorage()
                 },
                 setPassword: function(password) {
                     registration.password = password;
+                    updateLocalStorage()
                 },
                 setConfirmPassword: function(password) {
                     registration.confirmPassword = password;
+                    updateLocalStorage()
                 },
                 getUsername() {
                     return registration.username;
